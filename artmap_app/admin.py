26d7e364616e4ec2ed00c9aca78e7218b122009e
Александр Харук <a.haruk@gmail.com>
2022-06-07 23:45:52 +0300

    Add Place

--- artmap_app/admin.py
+++ artmap_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from artmap_app.models import Place
+
+
+class PlaceAdminForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        widgets = {
+            'description_short': forms.Textarea
+        }
+        fields = '__all__'
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    form = PlaceAdminForm
--- artmap_app/models.py
+++ artmap_app/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=127)
+    description_short = models.CharField(max_length=255)
+    description_long = models.TextField()
+
+    lng = models.DecimalField(
+        max_digits=17,
+        decimal_places=14,
+        validators=[
+            MinValueValidator(-180.0),
+            MaxValueValidator(180.0),
+        ]
+    )
+
+    lat = models.DecimalField(
+        max_digits=16,
+        decimal_places=14,
+        validators=[
+            MinValueValidator(-90.0),
+            MaxValueValidator(90.0),
+        ]
+    )
+
+    def __str__(self):
+        return self.title
+
+
