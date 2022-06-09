3f24728303384c1e59029de2abf798567eeba90c
Александр Харук <a.haruk@gmail.com>
2022-06-09 23:25:06 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media_dir/'
+MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
