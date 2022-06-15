a3caecc28645c0450be0d5634dcdcbf911713c19
Александр Харук <a.haruk@gmail.com>
2022-06-15 21:56:54 +0300

    Add inline preview

--- artmap_app/admin.py
+++ artmap_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from artmap_app.forms import PlaceAdminForm
 from artmap_app.models import *
@@ -6,6 +7,11 @@ from artmap_app.models import *
 
 class InlineImageAdmin(admin.TabularInline):
     model = Image
+    readonly_fields = ['image_preview']
+
+    @staticmethod
+    def image_preview(img_obj: Image):
+        return format_html('<img style="height:200px;width:auto;" src="{}" />', img_obj.pict.url)
 
 
 @admin.register(Place)
