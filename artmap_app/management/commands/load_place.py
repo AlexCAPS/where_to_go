767efb4dc7ba76afa06ce3e459a8bf6669bd5bb7
Александр Харук <a.haruk@gmail.com>
2022-06-20 03:11:16 +0300

    Reformat file and add support url data source for load_place command

--- artmap_app/management/commands/load_place.py
+++ artmap_app/management/commands/load_place.py
@@ -1,19 +1,18 @@
 import json
 from argparse import FileType
-from io import TextIOWrapper
 
 from django.core.management import BaseCommand
 from django.db import transaction
 
 from artmap_app.models import Place, Image
-from artmap_app.utils import download_image
+from artmap_app.utils import download_image, download_place_file
 
 
 class Command(BaseCommand):
     help = 'Load place from file(s)'
 
     def add_arguments(self, parser):
-        parser.add_argument('place_dump_files', nargs='+', type=FileType('r'))
+        parser.add_argument('place_dump_files', nargs='+', type=self.__json_source)
 
     def handle(self, *args, **options):
         for dumped_place in options['place_dump_files']:
@@ -26,19 +25,24 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('Import completed'))
 
-    @transaction.atomic
-    def import_place(self, dumped_place: TextIOWrapper):
-        place_content = json.load(dumped_place)
+    @staticmethod
+    def __json_source(arg_string):
+        if arg_string.startswith('http'):
+            return download_place_file(arg_string)
+        else:
+            return json.load(FileType('r')(arg_string))
 
+    @transaction.atomic
+    def import_place(self, place_content: dict):
         place = Place.load_place(place_content)
 
         if not place:
-            self.stdout.write(self.style.WARNING(f'File "{dumped_place.name}" was skipped.'))
+            self.stdout.write(self.style.WARNING('Place passed'))
             return
 
-        self.stdout.write(self.style.SUCCESS(f'Ploace from file "{dumped_place.name}" was loaded.'))
+        self.stdout.write(self.style.SUCCESS('Place info loaded'))
 
         for position, image_url in enumerate(place_content.get('imgs', [])):
             image_content = download_image(image_url)
             image = Image.load_image(place, image_content, position)
-            self.stdout.write(self.style.SUCCESS(f'Successfully loaded {image.pict.url}.'))
+            self.stdout.write(self.style.SUCCESS(f'Successfully loaded {image.pict.url}'))
--- artmap_app/utils.py
+++ artmap_app/utils.py
@@ -6,5 +6,6 @@ def download_image(url):
     return response.content
 
 
-if __name__ == '__main__':
-    download_image('https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/35cbdddf2799337d8b571d141edec616.JPG')
+def download_place_file(url):
+    response = requests.get(url)
+    return response.json()
